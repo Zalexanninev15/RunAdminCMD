3aaa644881c96bd4f0ea7e87e414137576990f01
Zalexanninev15 <blue.shark@disroot.org>
2020-08-14 19:04:09 +0300

    Source files

---
+++ RunAdminCMD/RunAdminCMD/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+#region Using directives
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ("RunAdminCMD")]
+[assembly: AssemblyDescription ("Utility for running files and scripts as an Administrator")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("RunAdminCMD")]
+[assembly: AssemblyCopyright ("GNU General Public License v3.0 by Zalexanninev15")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible (false)]
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all the values or you can use the default the Revision and 
+// Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion ("1.0.*")]
+[assembly: Guid ("d2318ff9-90ff-40c1-90cc-16fe359e7ae4")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
